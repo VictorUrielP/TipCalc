76963e9f2b411df78adfc08041211f8d80c1becc
UrielP <victorurielp@gmail.com>
2017-10-18 22:56:46 -0500

    Bind activity_main views using butterknife and butterknife Zelezny plug-in

--- app/src/main/java/com/example/windows/tipcalc/MainActivity.java
+++ app/src/main/java/com/example/windows/tipcalc/MainActivity.java
@@ -2,17 +2,39 @@ package com.example.windows.tipcalc;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.inputBill)
+    EditText inputBill;
+    @Bind(R.id.btnSubmit)
+    Button btnSubmit;
+    @Bind(R.id.inputPercentage)
+    EditText inputPercentage;
+    @Bind(R.id.btnIncrease)
+    Button btnIncrease;
+    @Bind(R.id.btnDecrease)
+    Button btnDecrease;
+    @Bind(R.id.btnClear)
+    Button btnClear;
+    @Bind(R.id.txtTip)
+    TextView txtTip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -23,13 +45,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.action_about){
+        if (item.getItemId() == R.id.action_about) {
             about();
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void about (){
+    private void about() {
         TipCalcApp app = (TipCalcApp) getApplication();
         String strUrl = app.getAboutUrl();
 
